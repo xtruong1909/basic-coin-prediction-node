5e7a6db79c964925b2035e4a3b745698c69e6a86
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2024-09-29 15:56:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,5 @@
 import os
-import json
 
-# Đảm bảo biến môi trường được thiết lập để chỉ đến tệp config.json
-config_file_path = os.getenv('CONFIG_FILE', 'config.json')
-
-# Đảm bảo tệp config.json tồn tại và có thể đọc được
-if not os.path.exists(config_file_path):
-    raise FileNotFoundError(f"Tệp config.json không tìm thấy tại: {config_file_path}")
-
-# Đọc tệp config.json
-with open(config_file_path, 'r') as file:
-    config = json.load(file)
-
-# Đặt biến data_base_path để sử dụng trong đoạn code của bạn
-data_base_path = config.get('data_base_path', 'default/path')
+app_base_path = os.getenv("APP_BASE_PATH", default=os.getcwd())
+data_base_path = os.path.join(app_base_path, "data")
+# model_file_path = os.path.join(data_base_path, "model.pkl")
