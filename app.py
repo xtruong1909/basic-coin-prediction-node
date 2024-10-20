f63a39f5b920fd6ab7af8ca800c760c64d8ec1cb
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2024-10-20 18:21:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,171 @@
-import json
-from flask import Flask, Response
-from model import download_data, format_data, train_model
-from model import forecast_price
+import os
+import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from config import data_base_path
+import random
+import requests
+import retrying
+import joblib
 
-app = Flask(__name__)
+forecast_price = {}
+
+binance_data_path = os.path.join(data_base_path, "binance/futures-klines")
+MAX_DATA_SIZE = 1000  # Giới hạn số lượng dữ liệu tối đa khi lưu trữ
+INITIAL_FETCH_SIZE = 1000  # Số lượng nến lần đầu tải về
+
+@retrying.retry(wait_exponential_multiplier=1000, wait_exponential_max=10000, stop_max_attempt_number=5)
+def fetch_prices(symbol, interval="1m", limit=1000, start_time=None, end_time=None):
+    try:
+        base_url = "https://fapi.binance.com"
+        endpoint = f"/fapi/v1/klines"
+        params = {
+            "symbol": symbol,
+            "interval": interval,
+            "limit": limit
+        }
+        if start_time:
+            params['startTime'] = start_time
+        if end_time:
+            params['endTime'] = end_time
+
+        url = base_url + endpoint
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(f'Failed to fetch prices for {symbol} from Binance API: {str(e)}')
+        raise e
+
+def download_data(token):
+    symbols = f"{token.upper()}USDT"
+    interval = "5m"
+    current_datetime = datetime.now()
+    download_path = os.path.join(binance_data_path, token.lower())
+    
+    file_path = os.path.join(download_path, f"{token.lower()}_5m_data.csv")
+
+    if os.path.exists(file_path):
+        start_time = int((current_datetime - timedelta(minutes=500)).timestamp() * 1000)
+        end_time = int(current_datetime.timestamp() * 1000)
+        new_data = fetch_prices(symbols, interval, 100, start_time, end_time)
+    else:
+        start_time = int((current_datetime - timedelta(minutes=INITIAL_FETCH_SIZE*5)).timestamp() * 1000)
+        end_time = int(current_datetime.timestamp() * 1000)
+        new_data = fetch_prices(symbols, interval, INITIAL_FETCH_SIZE, start_time, end_time)
+
+    new_df = pd.DataFrame(new_data, columns=[
+        "start_time", "open", "high", "low", "close", "volume", "close_time",
+        "quote_asset_volume", "number_of_trades", "taker_buy_base_asset_volume", 
+        "taker_buy_quote_asset_volume", "ignore"
+    ])
+
+    if os.path.exists(file_path):
+        old_df = pd.read_csv(file_path)
+        combined_df = pd.concat([old_df, new_df])
+        combined_df = combined_df.drop_duplicates(subset=['start_time'], keep='last')
+    else:
+        combined_df = new_df
+
+    if len(combined_df) > MAX_DATA_SIZE:
+        combined_df = combined_df.iloc[-MAX_DATA_SIZE:]
+
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+    combined_df.to_csv(file_path, index=False)
+    print(f"Updated data for {token} saved to {file_path}. Total rows: {len(combined_df)}")
+
+def format_data(token):
+    path = os.path.join(binance_data_path, token.lower())
+    file_path = os.path.join(path, f"{token.lower()}_5m_data.csv")
+
+    if not os.path.exists(file_path):
+        print(f"No data file found for {token}")
+        return
+
+    df = pd.read_csv(file_path)
+
+    columns_to_use = [
+        "start_time", "open", "high", "low", "close", "volume",
+        "close_time", "quote_asset_volume", "number_of_trades",
+        "taker_buy_base_asset_volume", "taker_buy_quote_asset_volume"
+    ]
+
+    if set(columns_to_use).issubset(df.columns):
+        df = df[columns_to_use]
+        df.columns = [
+            "start_time", "open", "high", "low", "close", "volume",
+            "end_time", "quote_asset_volume", "n_trades", 
+            "taker_volume", "taker_volume_usd"
+        ]
+        df.index = pd.to_datetime(df["start_time"], unit='ms')
+        df.index.name = "date"
+
+        output_path = os.path.join(data_base_path, f"{token.lower()}_price_data.csv")
+        df.sort_index().to_csv(output_path)
+        print(f"Formatted data saved to {output_path}")
+    else:
+        print(f"Required columns are missing in {file_path}. Skipping this file.")
+
+def train_model(token):
+    time_start = datetime.now()
+
+    price_data = pd.read_csv(os.path.join(data_base_path, f"{token.lower()}_price_data.csv"))
+    price_data["date"] = pd.to_datetime(price_data["date"])
+    price_data.set_index("date", inplace=True)
+    df = price_data.resample('10T').mean()
+
+    df = df.dropna()
+
+    # Define SARIMA parameters
+    order = (1, 1, 1)  # p, d, q
+    seasonal_order = (1, 1, 1, 12)  # P, D, Q, s (assuming yearly seasonality for monthly data)
+
+    try:
+        model = SARIMAX(df['close'], order=order, seasonal_order=seasonal_order, enforce_stationarity=False, enforce_invertibility=False)
+        sarima_model = model.fit(disp=False)
+    except Exception as e:
+        raise RuntimeError(f"An error occurred while fitting the SARIMA model: {e}")
+
+    # Save the model
+    joblib.dump(sarima_model, f'{token.lower()}_sarima_model.pkl')
+
+    # Forecasting next value
+    forecast_steps = 1
+    forecast = sarima_model.get_forecast(steps=forecast_steps)
+    forecast_mean = forecast.predicted_mean.iloc[-1]
+
+    # Xử lý biên độ ngẫu nhiên
+    now = datetime.now().astimezone()  # Giờ địa phương
+    is_weekend = now.weekday() >= 5  # Thứ 7, Chủ nhật
+    is_night = now.hour >= 19 or now.hour < 7  # Từ 19 giờ đến 7 giờ sáng
+
+    adjusted_price = calculate_random_fluctuation(forecast_mean, is_weekend, is_night)
+
+    forecast_price[token] = adjusted_price
+
+    print(f"Forecasted price for {token}: {forecast_price[token]}")
+
+    time_end = datetime.now()
+    print(f"Time elapsed forecast: {time_end - time_start}")
+
+def calculate_random_fluctuation(predicted_price, is_weekend, is_night):
+    if is_weekend and is_night:
+        fluctuation = random.uniform(-0.0015 * predicted_price, 0.0015 * predicted_price)  # 0% to 0.15%
+    elif is_night:
+        fluctuation = random.uniform(-0.003 * predicted_price, 0.003 * predicted_price)  # 0% to 0.3%
+    else:
+        fluctuation = random.uniform(-0.0005 * predicted_price, 0.0005 * predicted_price)  # 0% to 0.05%
+    
+    return predicted_price + fluctuation
 
 def update_data():
-    """Download price data, format data and train model."""
     tokens = ["ETH", "BTC", "BNB", "SOL", "ARB"]
     for token in tokens:
         download_data(token)
         format_data(token)
         train_model(token)
 
-def get_token_inference(token):
-    return forecast_price.get(token, 0)
-
-@app.route("/inference/<string:token>")
-def generate_inference(token):
-    """Generate inference for given token."""
-    if not token or token not in ["ETH", "BTC", "BNB", "SOL", "ARB"]:
-        error_msg = "Token is required" if not token else "Token not supported"
-        return Response(json.dumps({"error": error_msg}), status=400, mimetype='application/json')
-
-    try:
-        inference = get_token_inference(token)
-        return Response(str(inference), status=200)
-    except Exception as e:
-        return Response(json.dumps({"error": str(e)}), status=500, mimetype='application/json')
-
-@app.route("/update")
-def update():
-    """Update data and return status."""
-    try:
-        update_data()
-        return "0"
-    except Exception:
-        return "1"
-
 if __name__ == "__main__":
     update_data()
-    app.run(host="0.0.0.0", port=8011)
