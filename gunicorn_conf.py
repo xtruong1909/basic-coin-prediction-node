c2e5eea8f6c1ad196e3a35a699e5a02d2771f865
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2024-10-30 11:24:39 +0700

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,3 +1,5 @@
+import os  # Import os module to use environment variables
+
 # Gunicorn config variables
 loglevel = "info"
 errorlog = "-"  # stderr
@@ -9,7 +11,7 @@ keepalive = 5
 worker_class = "gthread"
 workers = int(os.getenv("WORKERS", "2"))  # Adjust based on environment variable
 threads = int(os.getenv("THREADS", "4"))  # Adjust based on environment variable
-bind = "0.0.0.0:9000"
+bind = "0.0.0.0:8011"  # Update port to match docker-compose.yml
 preload_app = True  # Preload the application to reduce worker startup time
 max_requests = 1000  # Limit the number of requests a worker will handle before restarting
 max_requests_jitter = 100  # Add jitter to avoid workers restarting simultaneously
