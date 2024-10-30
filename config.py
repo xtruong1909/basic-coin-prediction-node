b0b8aa2a9bb0f8ac4607f7975c5bd416257f7fe2
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2024-10-30 17:49:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,3 @@ import os
 app_base_path = os.getenv("APP_BASE_PATH", default=os.getcwd())
 data_base_path = os.path.join(app_base_path, "data")
 # model_file_path = os.path.join(data_base_path, "model.pkl")
-SARIMA_ORDER = (1, 1, 1)
-SARIMA_SEASONAL_ORDER = (1, 1, 1, 12)
-MAX_FORECAST_STEPS = 1  # Dự báo cho một bước tiếp theo
