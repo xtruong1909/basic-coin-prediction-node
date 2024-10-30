1a65e54038635edd1813cd3b434c79f3a7cf2df5
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2024-10-30 11:04:41 +0700

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,17 +1,12 @@
-import os  # Thêm dòng này để sử dụng os.getenv()
-
 # Gunicorn config variables
 loglevel = "info"
 errorlog = "-"  # stderr
 accesslog = "-"  # stdout
 worker_tmp_dir = "/dev/shm"
 graceful_timeout = 120
-timeout = 100  # Increased timeout to handle longer SARIMA model processing
+timeout = 30
 keepalive = 5
 worker_class = "gthread"
-workers = int(os.getenv("WORKERS", "2"))  # Adjust based on environment variable
-threads = int(os.getenv("THREADS", "4"))  # Adjust based on environment variable
-bind = "0.0.0.0:8011"
-preload_app = True  # Preload the application to reduce worker startup time
-max_requests = 1000  # Limit the number of requests a worker will handle before restarting
-max_requests_jitter = 100  # Add jitter to avoid workers restarting simultaneously
+workers = 1
+threads = 8
+bind = "0.0.0.0:9000"
