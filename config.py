d19a22198597a9167e40af9f7995b596fa7cac71
xtruong1909 <140407808+xtruong1909@users.noreply.github.com>
2024-11-03 20:42:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 import os
 
+# Set base path for the application
 app_base_path = os.getenv("APP_BASE_PATH", default=os.getcwd())
+
+# Define data base path for storing data
 data_base_path = os.path.join(app_base_path, "data")
-# model_file_path = os.path.join(data_base_path, "model.pkl")
+
+# Define model directory for SARIMA model files
+model_directory = os.path.join(data_base_path, "models")
+
+# Environment configurations for worker and model
+worker_loop_seconds = int(os.getenv("WORKER_LOOP_SECONDS", 60))
+model_name = os.getenv("MODEL_NAME", "sarima")
